264a8b91f7311546b6ea9adedbcf1846b9314bbf
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-10-26 16:34:54 -0500

    Update install-hyprland.sh

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-
-#_____________________________________________________________________________
+#!/bin/sh
 
 echo "Installing wayland and needed packages..."
-sudo pkg install wayland seatd socket wlroots drm-510-kmod elogind
+sudo pkg install wayland seatd socket wlroots drm-510-kmod drm-kmod elogind
+sudo pkg install python py39-ranger btop neofetch micro
 
 echo "Enabling Services..."
 export XDG_RUNTIME_DIR=/var/run/user/`id -u`
@@ -14,40 +13,10 @@ sudo sysrc elogind_enable="YES"
 sudo pw groupmod video -m cinnamon
 sudo echo "kld_list="drm-510-kmod"" >> /etc/rc.conf
 
-#_____________________________________________________________________________
-
-echo "Would you like to install optional packages? (yes/no)"
-read response
-
-if [[ "$response" == "yes" ]]; then
-    echo "Installing optional packages..."
-    sudo pkg install py39-ranger btop neofetch micro
-elif [[ "$response" == "no" ]]; then
-    echo "Okay, skipping..."
-else
-    echo "Invalid response. Please enter 'yes' or 'no'"
-fi
-
-#_____________________________________________________________________________
-
-echo "Would you like to install other tools? (yes/no)"
-read response
-
-if [[ "$response" == "yes" ]]; then
-    echo "Installing optional packages..."
-    sudo pkg install python
-elif [[ "$response" == "no" ]]; then
-    echo "Okay, skipping..."
-else
-    echo "Invalid response. Please enter 'yes' or 'no'"
-fi
-
-#_____________________________________________________________________________
-
-#Still need to add all packages to install, UI configs, and Wifi drivers, to the script
-
 sudo sysrc kld_list+=i915kms
 
 sudo sysrc dbus_enable="YES"
 
 sudo pw groupmod video -m cinnamon
+
+#Still need to add all packages to install, UI configs, and Wifi drivers, to the script
