3597ab421d05404fd020e9a7d813295529d61c91
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-10-26 16:55:36 -0500

    Update install.sh

--- Hyprland/install.sh
+++ Hyprland/install.sh
@@ -28,3 +28,21 @@ mkdir -p "$HOME/.config/dunst"
 mv "dunstrc" "/$HOMW/.config/dunst"
 
 #Still need to add all packages to install, UI configs, polkit, Wifi drivers, network selector, and bluetooth, to the script
+
+#Unsure about these commands
+
+#export MOZ_ENABLE_WAYLAND=1
+#export XDG_SESSION_TYPE=wayland
+#export XDG_RUNTIME_DIR=/tmp/$USER/.xdg
+#export CLUTTER_BACKEND=wayland
+#export SDL_VIDEODRIVER=wayland
+#export LIBGL_DRI3_ENABLE=1
+#export GDK_BACKEND=wayland
+#export XKB_DEFAULT_RULES=evdev
+#export QT_QPA_PLATFORM=wayland-egl
+#export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+#export BEMENU_BACKEND=wayland
+#export WLR_DRM_NO_ATOMIC=1
+#export XCURSOR_THEME=whiteglass
+#[ -d $XDG_RUNTIME_DIR ] || mkdir -m 700 -p $XDG_RUNTIME_DIR
+#exec ck-launch-session dbus-launch --sh-syntax --exit-with-session Hyprland
