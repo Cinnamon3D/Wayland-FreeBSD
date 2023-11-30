f11e0b44db98e02f26c856a858d1df1f89d193c7
Simeon Black <90055606+Cinnamon3D@users.noreply.github.com>
2023-11-30 17:01:42 -0600

    Update install.sh

--- KDE/install.sh
+++ KDE/install.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-echo "Installing wayland and needed packages..."
-sudo pkg install plasma5-plasma firefox drm-kmod kate dolphin python py39-ranger btop neofetch micro calcurse wifimgr
+sudo pkg install plasma5-plasma konsole firefox drm-kmod sddm kate dolphin python py39-ranger btop neofetch micro calcurse wifimgr
+sudo pkg remove plasma5-discover
 
 sudo pw groupmod video -m cinnamon
 
 sudo sysrc kld_list+=i915kms
 
 sudo sysrc dbus_enable="YES"
+
+sudo sysrc sddm_enable="YES"
+
+sudo sysrc sddm_lang="en_ES"
+
+echo "exec ck-launch-session startplasma-x11" > ~/.xinitrc
